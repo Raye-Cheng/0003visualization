dc0e8b6385b9215aa30d3e2bd767eb3f806fa2a7
Raye-Cheng <raynecheng7@gmail.com>
2023-05-24 08:21:52 +0100

    About new map

--- Singapore_IndustryPopulation/financial/finan.js
+++ Singapore_IndustryPopulation/financial/finan.js
@@ -1,3 +1,7 @@
+import * as echarts from 'echarts';
+// Please ensure BaiduMap script has been loaded before importing bmap extension
+// https://api.map.baidu.com/api?v=3.0&ak=YOUR_APP_KEY
+import 'echarts/extension/bmap/bmap';
 var chartDom = document.getElementById('main');
 var myChart = echarts.init(chartDom);
 var option;
@@ -29,20 +33,21 @@ const data = [
 const geoCoordMap = {
     "Brunei Darussalam": [4.4137155, 114.5653908], "Mainland China": [19.1153424, 72.9085796], "Hong Kong": [22.2793278, 114.1628131], "India": [22.3511148, 78.6677428], "Indonesia": [-2.4833826, 117.8902853], "Israel": [30.8124247, 34.8594762], "Japan": [36.5748441, 139.2394179], "Republic Of Korea": [36.638392, 127.6961188], "Malaysia": [4.5693754, 102.2656823], "Myanmar": [17.1750495, 95.9999652], "Philippines": [12.7503486, 122.7312101], "Taiwan": [23.9739374, 120.9820179], "Thailand": [14.8971921, 100.83273], "Vietnam": [15.9266657, 107.9650855], "Austria": [47.59397, 14.12456], "Belgium": [50.6402809, 4.6667145], "Denmark": [55.670249, 10.3333283], "Finland": [63.2467777, 25.9209164], "France": [46.603354, 1.8883335], "Germany": [51.1638175, 10.4478313], "Ireland": [52.865196, -7.9794599], "Luxembourg": [49.8158683, 6.1296751], "Netherlands": [52.2434979, 5.6343227], "Norway": [61.1529386, 8.7876653], "    Switzerland": [46.7985624, 8.2319736], "United Kingdom": [54.7023545, -3.2765753], "United States": [39.7837304, -100.445882], "Canada": [61.0666922, -107.991707], "Australia": [-24.7761086, 134.755], "New Zealand": [-41.5000831, 172.8344077]
 };
+
 const convertData = function (data) {
   var res = [];
   for (var i = 0; i < data.length; i++) {
-    var geoCoord = geoCoordMap[data[i].name];
+    var name = data[i].name;
+    var geoCoord = geoCoordMap[name];
     if (geoCoord) {
       res.push({
-        name: data[i].name,
+        name: name,
         value: geoCoord.concat(data[i].value)
       });
     }
   }
   return res;
 };
-
 
 option = {  
   tooltip: {
@@ -73,11 +78,11 @@ option = {
     },
   ],
   geo: {
-    map: 'regions',
+    map:map,
     label: {
         show: false
     },
-    room: true,
+    // room: true,
     itemStyle: {
         areaColor: '#f3f3f3',
         borderColor: '#333'

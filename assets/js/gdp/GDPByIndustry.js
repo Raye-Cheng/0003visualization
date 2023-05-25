cf935578c9224f3ab451a024de5c56f33fe3a119
yujingDING <115044837+yujingDING@users.noreply.github.com>
2023-05-25 22:58:49 +0100

    update color v1

--- assets/js/gdp/GDPByIndustry.js
+++ assets/js/gdp/GDPByIndustry.js
@@ -8,10 +8,10 @@ function deleteGDPByIndustry() {
 
 function showGDPByIndustry(elementId) {
 
-  // 基于准备好的dom，初始化echarts实例
+  // Initialize the echarts instance based on the prepared dom
   GDPByIndustry = echarts.init(document.getElementById(elementId));
 
-  // 指定图表的配置项和数据
+  // Specifies the configuration items and data for the chart
   option = {
     title: {
         text: 'Stacked Area Chart',
@@ -67,6 +67,11 @@ function showGDPByIndustry(elementId) {
             emphasis: {
                 focus: 'series'
             },
+            itemStyle:{
+                normal:{
+                    color:'#F46FA1'
+                }
+            },
             data: [0.09884, 0.08756, 0.07493, 0.0646, 0.05857, 0.05762, 0.051,
                 0.04545, 0.0437, 0.0428, 0.03825, 0.03669, 0.0353, 0.03648, 0.03653,
                 0.0346, 0.03363, 0.03276, 0.03179, 0.03401, 0.03478, 0.03558]
@@ -78,6 +83,11 @@ function showGDPByIndustry(elementId) {
             areaStyle: {},
             emphasis: {
                 focus: 'series'
+            },
+            itemStyle:{
+                normal:{
+                    color:'#77BC4D'
+                }
             },
             data: [34.82106, 32.29967, 32.43431, 31.93841, 33.55911, 32.87524,
                 32.24201, 29.9291, 27.86289, 28.41936, 28.18147, 26.7815, 26.54478,
@@ -91,6 +101,11 @@ function showGDPByIndustry(elementId) {
             areaStyle: {},
             emphasis: {
                 focus: 'series'
+            },
+            itemStyle:{
+                normal:{
+                    color:'#F3C55E'
+                }
             },
             data: [65.0801, 67.61277, 67.49075, 67.99699, 66.38232, 67.06714, 67.707,
                 70.02545, 72.09341, 71.53784, 71.78028, 73.18181, 73.41992, 75.08781,
